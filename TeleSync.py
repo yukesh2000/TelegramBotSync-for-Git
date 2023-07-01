46a324e2216a3fb51e5c6129bf69fd83e21eda14
darrenlsx <darren.lim@live.com.sg>
2023-07-01 17:13:57 +0800

    Update UI and fix bugs

--- TeleSync.py
+++ TeleSync.py
@@ -10,6 +10,7 @@ from googleapiclient.errors import HttpError
 
 ## Set up Telegram Bot
 import telebot
+from telebot import types
 TOKEN = ""
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
@@ -37,42 +38,57 @@ if not creds or not creds.valid:
 
 service = build('calendar', 'v3', credentials=creds)
 
-## List of Commands
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    reply = bot.send_message(message.chat.id, "Hello, can you type song name?")
+# List of Commands
+@bot.message_handler(commands=['add'])
+def create_event(message):
+    reply = bot.send_message(message.chat.id, "What is the song title to add?")
     def get_song_name(message):
         global song_name
         song_name = message.text
         print(song_name)
-
+        bot.send_message(message.chat.id, "Okai adding for song title: {}\n\nPlease type in the dates in the proper format\nDate: DD/MM _(day)_\nTime: HH.MMam/pm - HH.MMam/pm\nCMI:\nAgenda:\n\nYou may leave CMI or Agenda empty.\nThe (day) is optional, doesnt affect the bot, just for your ez ref if want to paste in grpchat.\nIf you need an example enter /eg and go back to /add again to continue.\nYou can copy the below message to get started.".format(song_name), parse_mode="Markdown")
+        bot.send_message(message.chat.id, "Date:\nTime:\nCMI:\nAgenda:\n ")
     bot.register_next_step_handler(reply, get_song_name)
 
-@bot.message_handler(commands=['create'])
-def create_event(message):
-    bot.send_message(message.chat.id, "adding")
-    service = build('calendar', 'v3', credentials=creds)
-    bot.send_message(message.chat.id, "servicing")
-    event = {
-        'summary': 'event1?',
-        'description': 'sb cmi',
-        'start': {
-            'dateTime': '2023-06-30T21:00:00+08:00',
-            'timeZone': 'Asia/Singapore',
-        },
-        'end': {
-            'dateTime': '2023-06-30T22:00:00+08:00',
-            'timeZone': 'Asia/Singapore',
-        },
-    }
+@bot.message_handler(commands=['start'])
+def welcome_message(message):
+    bot.send_message(message.chat.id, "Hello, welcome to N.Trance Sync, enter /menu to begin :>")
 
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    bot.send_message(message.chat.id, "Event added")
+@bot.message_handler(commands=['menu'])
+def handle_m(message):
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    b1 = telebot.types.InlineKeyboardButton('/add: Add Date(s)', callback_data='add')
+    b2 = telebot.types.InlineKeyboardButton('/eg: Example Format', callback_data='eg')
+    b3 = telebot.types.InlineKeyboardButton('/delete: Delete Date', callback_data='delete')
+    keyboard.row(b1)
+    keyboard.row(b2)
+    keyboard.row(b3)
+    commands = "Here are the commands :)\n/start   : Welcome message\n\n/add     : Prompts user for song title and dates to add to calendar\n\n/eg       : Provides an example in proper format/delete : Prompts user to choose which song title to delete from and will be given a list of song dates currently registered in calendar\n\n/delete : Prompts user to choose which song title to delete from and will be given a list of song dates currently registered in calendar\n"
+
+    bot.send_message(message.chat.id, commands,reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda c:True)
+def handle_menu_click(c):
+    if (c.data == "add"):
+        create_event(c.message)
+    elif (c.data == "eg"):
+        example_message(c.message)
+    elif (c.data == "delete"):
+        delete_event(c.message)
+
+# @bot.message_handler(commands=['help'])
+# def help_message(message):
+#     bot.send_message(message.chat.id, "/start   : Welcome message\n/add     : Prompts user for song title and dates to add to calendar\n/delete : Prompts user to choose which song title to delete from and will be given a list of song dates currently registered in calendar\n/eg       : Provides an example in proper format")
+
+@bot.message_handler(commands=['eg'])
+def example_message(message):
+    bot.send_message(message.chat.id, "Date: 01/02\nTime: 9.00am - 10.30pm\nCMI: IU :/ she'll come after 8pm (hopefully)\nAgenda: Clean till chorus\n\nDate: 02/02\nTime: 10.00am - 12.00pm\nCMI: Full crew\nAgenda: Filming at MBS")
 
 @bot.message_handler(commands=['delete'])
 def delete_event(message):
     reply = bot.send_message(message.chat.id, "Which song title you want to delete from?")
-    
+
     def handle_song_name_reply(message):
         global service
         try:
@@ -88,12 +104,12 @@ def delete_event(message):
 
             message_list = ""
             filtered_id_list  = []
-
+            counter = 0
             for i in range(len(events)):
                 if (events[i]['summary'].lower().replace(" ", "") == message.text.lower().replace(" ","")):
-                    message_list += str(i+1) + ") " + rfc3339_to_GMT_converter(events[i]['start'].get('dateTime', events[i]['start'].get('date')), events[i]['end'].get('dateTime', events[i]['end'].get('date')))+ "\n"
+                    message_list += str(counter+1) + ") " + rfc3339_to_GMT_converter(events[i]['start'].get('dateTime', events[i]['start'].get('date')), events[i]['end'].get('dateTime', events[i]['end'].get('date')))+ "\n"
                     filtered_id_list.append(events[i]['id'])
-            
+
             if (len(filtered_id_list) == 0):
                 bot.send_message(message.chat.id, "Sorry song name not found :( pls type /delete again")
                 return
@@ -117,9 +133,9 @@ def delete_event(message):
 
         except HttpError as error:
             print('An error occurred: %s' % error)
-        
+
     bot.register_next_step_handler(reply, handle_song_name_reply)
-    
+
 
 def rfc3339_to_GMT_converter(start, end):
     date = start[8:10]+"/"+start[5:7]
@@ -131,14 +147,18 @@ def time_check(s):
     if (time24hr_hr >= 12):
         time24hr_hr -= 12
         return str(time24hr_hr) + "." + time_min + "pm"
-    return str(time24hr_hr) + "." + time_min + "am" 
+    return str(time24hr_hr) + "." + time_min + "am"
 
+# Prepares given input events from user
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
+def event_handler(message):
   if ("Date" in message.text):
-    handle_message(message.text)
+    handle_message(message.text, message)
+  else:
+      bot.send_message(message.chat.id, "Sorry me no understand :( Please type in correct command")
 
-def handle_message(text):
+# Handles given input events from user
+def handle_message(text, message):
     lines = text.splitlines()
     event_list = []
     dict = {}
@@ -150,28 +170,36 @@ def handle_message(text):
         # Next event/Exit
         if (len(lines[i]) <= 1 or i == len(lines) - 1):
             event_list.append(Event(dict["Date"], dict["Time"], dict["CMI"], dict["Agenda"]))
+            # print(dict)
             dict = {}
 
     global service
-    for e in event_list:
-        global song_name
+    try:
+        for e in event_list:
 
-        time_converted = date_converter(e.date, e.time)
-        event = {
-            'summary': '{}'.format(song_name) ,
-            'description': 'CMI: {}\nAgenda: {}'.format(e.cmi, e.agenda),
-            'start': {
-                'dateTime': '{}'.format(time_converted[0]),
-                'timeZone': 'Asia/Singapore',
-            },
-            'end': {
-                'dateTime': '{}'.format(time_converted[1]),
-                'timeZone': 'Asia/Singapore',
-            },
-        }
-        service.events().insert(calendarId='primary', body=event).execute()
-        print(event)
 
+            global song_name
+
+            time_converted = date_converter(e.date, e.time)
+            event = {
+                'summary': '{}'.format(song_name) ,
+                'description': 'CMI: {}\nAgenda: {}'.format(e.cmi, e.agenda),
+                'start': {
+                    'dateTime': '{}'.format(time_converted[0]),
+                    'timeZone': 'Asia/Singapore',
+                },
+                'end': {
+                    'dateTime': '{}'.format(time_converted[1]),
+                    'timeZone': 'Asia/Singapore',
+                },
+            }
+            service.events().insert(calendarId='primary', body=event).execute()
+            bot.send_message(message.chat.id, "Okay added into calendar")
+            print(event)
+    except (HttpError, IndexError) as error:
+        bot.send_message(message.chat.id, "Sorry incorrect format. Please check again and start from /add")
+
+# Converts date-time to rf3339 format
 def date_converter(date, time):
     spliced_day = date[:2]
     spliced_month = date[3:5]
@@ -185,6 +213,7 @@ def date_converter(date, time):
 
     return [start_string, end_string]
 
+# Splices given date-time
 def handle_time(s):
     arr = s.strip().split("-")
     return [parse_time(arr[0]), parse_time(arr[1])]
